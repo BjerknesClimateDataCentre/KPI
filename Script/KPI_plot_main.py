3b41a78aaceb94ba487289dffb212a845967ec97
Camilla <camilla.landa@uib.no>
2020-02-12 08:59:46 +0100

    (#4) Add output directory and plot of one parameter

--- Script/KPI_plot_main.py
+++ Script/KPI_plot_main.py
@@ -13,7 +13,12 @@
 
 import quince_kpi as kpi
 import os
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import seaborn as sns    # Do I need this???
+
 
 ###----------------------------------------------------------------------------
 ### Handling directories
@@ -22,12 +27,15 @@ import pandas as pd
 # Store path to the main script directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-# Create a data directory if it does not already exist
-if not os.path.isdir('./data_files'):
-	os.mkdir(os.path.join(script_dir,'data_files'))
+# Create a data files and output directories if does not exist
+directories = ["data_files", "output"]
+for directory in directories:
+	if not os.path.isdir('./'+ directory):
+		os.mkdir(os.path.join(script_dir,directory))
 
-# Store path to the data directory
+# Store path to the data and output directories
 data_dir = os.path.join(script_dir,'data_files')
+output_dir = os.path.join(script_dir,'output')
 
 
 ###----------------------------------------------------------------------------
@@ -48,6 +56,7 @@ data_files = os.listdir(data_dir)
 ###----------------------------------------------------------------------------
 
 # Loop through all files in data directory and read data into 'df'
+# !!! If more than one file: add the df's together (NaN's if some cols missing)
 for file in data_files:
 	data_path = os.path.join(data_dir, file)
 	df = pd.read_csv(data_path, low_memory=False)
@@ -62,4 +71,43 @@ parameters = kpi.get_parameters(df)
 ###----------------------------------------------------------------------------
 ### Make KPI plots
 ###----------------------------------------------------------------------------
+
+#-------------
+# USefull stuff:
+#--------
+
+# Get all unique values for a column:
+# df['Colname'].unique()
+
+# Remove rows with NaN/'empty'
+# df = df.dropna(subset=['pCO2 [uatm]'])
+
+# Group data:
+# group_by_QCFlag = df.groupby(by=['colname QC Flag'])
+
+# Get average and count after grouping:
+# flag_average = group_by_QCFlag.mean()
+# flag_count = group_by_QCFlag.count()
+
+# Plot function - plot() with parameters:
+# kind - bar, barh, pie, scatter, kde
+# color - in hex codes
+# linestyle - solid, dotted, dashed
+# xlim, ylim
+# legend - true or false
+# labels - list corresponding to the number of cols in data frame
+# title - a string
+#------------
+
+
+
+# Plot one variable
+
+df_pco2_noNaN = df.dropna(subset=['pCO2 [uatm]'])
+df_pco2_noNaN.plot(kind='scatter', y='pCO2 [uatm]', x='Date/Time')
+plt.savefig(output_dir + '/pco2.png')
+
+
+
+
 
