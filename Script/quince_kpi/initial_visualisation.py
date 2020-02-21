63084ae1883f445b32280f3fcb2a3711158519d9
Camilla <camilla.landa@uib.no>
2020-02-21 11:21:38 +0100

    (#4) 'Plot data' function always creates one figure

--- Script/quince_kpi/initial_visualisation.py
+++ Script/quince_kpi/initial_visualisation.py
@@ -15,59 +15,89 @@ import string
 # Function plots parameter(s) vs time and saves plot(s) in output directory
 def plot_data(colnames, df, output_dir, cleaned=False):
 
-	# List of letters used for labelling the plots
-	letters=list(string.ascii_lowercase[0:len(colnames)])
-	letter_count = 0
+	# Get number of columns to plot
+	ncol=len(colnames)
 
-	# Create the filepaths list which will be returned
-	filenames = []
-	for colname in colnames:
+	if ncol==1:
 
 		# Remove rows with NaNs
-		df = df.dropna(subset=[colname])
+		df = df.dropna(subset=[colnames[0]])
 
-		# Identify the parameters QC flag columns and set the color code
-		color_column = colname + ' QC Flag'
+		# Identify the parameters QC flag columns
+		color_column = colnames[0] + ' QC Flag'
 
-		# Setup the plot
-		fig, ax = plt.subplots()
+		# Set up the plot
+		fig, ax = plt.subplots(figsize= (9,3*ncol))
 
-		# If cleaned is false, plot all data. Else, only plot data with QC flag 2.
+		# If cleaned is false, plot all data. Else, plot data with QC flag 2.
 		if cleaned is False:
 			# Set plot color for the QC flags, and add to plot in a loop
 			color_code = {2:'green',3:'orange',4:'red'}
 			for flag, col in color_code.items():
 				limited_df = df[df[color_column]==flag]
-				ax.scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+				ax.scatter(x=limited_df['Date/Time'], y=limited_df[colnames[0]],
 					c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
-			ax.legend(fontsize=11)
-
-
+				ax.legend(fontsize=9, bbox_to_anchor=(1, 1))
 		else:
 			limited_df = df[df[color_column]==2]
-			ax.scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+			ax.scatter(x=limited_df['Date/Time'], y=limited_df[colnames[0]],
 				c='green', alpha=0.7, edgecolors='none', marker='.')
 
-		# Make axis sideways, add axis labels, grid and lettering
+		# Add axis labels and grid
+		plt.ylabel(colnames[0], fontsize=10)
 		fig.autofmt_xdate()
-		plt.xlabel('Date/Time', fontsize=13)
-		plt.ylabel(colname, fontsize=13)
 		ax.grid(True)
-		if len(colnames) > 1:
-			ax.set_title(letters[letter_count] + ')', loc='left', fontsize=15,
-				fontweight="bold")
 
-		# Save plot to file
-		if cleaned is True:
-			filename = colname.split(' [')[0] + '_cleaned.png'
-		else:
-			filename = colname.split(' [')[0] + '.png'
-		filename = filename.replace(' ','_')
-		filepath = os.path.join(output_dir, filename)
-		plt.savefig(filepath)
+	else:
+		# List of letters used for labelling the plots
+		letters=list(string.ascii_lowercase[0:ncol])
 
-		# Add filepath to paths list and increase letter counter
-		filenames.append(filename)
-		letter_count += 1
+		# Setup the plot, and make subplots in a loop
+		fig, ax = plt.subplots(len(colnames), sharex=True, figsize= (9,3*ncol))
+		count = 0
+		for colname in colnames:
 
-	return filenames
+			# Remove rows with NaNs
+			df = df.dropna(subset=[colname])
+
+			# Identify the parameters QC flag columns and set the color code
+			color_column = colname + ' QC Flag'
+
+			# If cleaned is false, plot all data. Else, plot data with QC flag 2.
+			if cleaned is False:
+				# Set plot color for the QC flags, and add to plot in a loop
+				color_code = {2:'green',3:'orange',4:'red'}
+				for flag, col in color_code.items():
+					limited_df = df[df[color_column]==flag]
+					ax[count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+						c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
+				if count == 0:
+					ax[count].legend(fontsize=9, bbox_to_anchor=(1, 1))
+			else:
+				limited_df = df[df[color_column]==2]
+				ax[count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+					c='green', alpha=0.7, edgecolors='none', marker='.')
+
+			# Add y-axis label, grid and lettering
+			ax[count].set_ylabel(colname, fontsize=10)
+			ax[count].grid(True)
+			if len(colnames) > 1:
+				ax[count].set_title(letters[count] + ')', loc='left', fontsize=10,
+					fontweight="bold")
+
+			count += 1
+
+		# Add x-axis sideways
+		fig.autofmt_xdate()
+
+
+	#----------------------
+	# Save plot to file
+	if cleaned is True:
+		filename = 'kpi_plot_data_cleaned.png'
+	else:
+		filename = 'kpi_plot_data.png'
+	filepath = os.path.join(output_dir, filename)
+	plt.savefig(filepath, bbox_inches='tight')
+
+	return filename
