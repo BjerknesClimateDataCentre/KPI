fc8130536810538b29a7a5276678593dd7a8b714
Camilla <camilla.landa@uib.no>
2020-02-24 14:55:15 +0100

    (#4) Complete re-structure of 'plot data' function (fewer repetitions)

--- Script/quince_kpi/initial_visualisation.py
+++ Script/quince_kpi/initial_visualisation.py
@@ -13,143 +13,110 @@ import string
 import math
 
 
+def make_subplot(colname, df, cleaned, ax):
+
+	# Remove rows with NaNs
+	df = df.dropna(subset=[colname])
+
+	# Identify the parameters QC flag columns
+	color_column = colname + ' QC Flag'
+
+	# If cleaned is false, plot all data. Else, plot data with QC flag 2.
+	if cleaned is False:
+		# Set plot color for the QC flags and plot one flag at the time
+		color_code = {2:'green',3:'orange',4:'red'}
+		for flag, col in color_code.items():
+				limited_df = df[df[color_column]==flag]
+				ax.scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+					c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
+
+# HOW TO ADD LEGENG WITH FLAG CODES
+				#ax.legend(fontsize=9, bbox_to_anchor=(1, 1))
+		#if letter_count == 0:
+			#ax.legend(fontsize=9, bbox_to_anchor=(1, 1))
+		#if row_count == 0 and col_count == 1:
+			#ax.legend(fontsize=9, bbox_to_anchor=(1, 1))
+
+		else:
+			limited_df = df[df[color_column]==2]
+			ax.scatter(x=limited_df['Date/Time'], y=limited_df[colname],
+				c='green', alpha=0.7, edgecolors='none', marker='.')
+
+	ax.grid(True)
+
+
 # Function plots parameter(s) vs time and saves plot(s) in output directory
 def plot_data(colnames, df, output_dir, cleaned=False):
+
+	# Decide limit when plot in two columns:
+	plot_limit = 4
 
 	# Get number of columns to plot
 	n_plot=len(colnames)
 
-	#------
-	if n_plot == 1:
-
-		# Remove rows with NaNs
-		df = df.dropna(subset=[colnames[0]])
-
-		# Identify the parameters QC flag columns
-		color_column = colnames[0] + ' QC Flag'
-
-		# Set up the plot
-		fig, ax = plt.subplots(figsize= (9,3*n_plot))
-
-		# If cleaned is false, plot all data. Else, plot data with QC flag 2.
-		if cleaned is False:
-			# Set plot color for the QC flags, and add to plot in a loop
-			color_code = {2:'green',3:'orange',4:'red'}
-			for flag, col in color_code.items():
-				limited_df = df[df[color_column]==flag]
-				ax.scatter(x=limited_df['Date/Time'], y=limited_df[colnames[0]],
-					c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
-				ax.legend(fontsize=9, bbox_to_anchor=(1, 1))
-		else:
-			limited_df = df[df[color_column]==2]
-			ax.scatter(x=limited_df['Date/Time'], y=limited_df[colnames[0]],
-				c='green', alpha=0.7, edgecolors='none', marker='.')
-
-		# Add axis labels and grid
-		#plt.ylabel(colnames[0], fontsize=10) Try this instead:
-		plt.title('     ' + colnames[0], fontsize=10, fontweight='bold')
-		fig.autofmt_xdate()
-		ax.grid(True)
-
-	#------
-	elif n_plot > 1 and n_plot <= 5:
-		# List of letters used for labelling the plots
-		letters=list(string.ascii_lowercase[0:n_plot])
-
-		# Setup the plot, and make subplots in a loop
-		fig, ax = plt.subplots(n_plot, sharex=True, figsize= (9,3*n_plot))
-		count = 0
-		for colname in colnames:
-
-			# Remove rows with NaNs
-			df = df.dropna(subset=[colname])
-
-			# Identify the parameters QC flag columns and set the color code
-			color_column = colname + ' QC Flag'
-
-			# If cleaned is false, plot all data. Else, plot data with QC flag 2.
-			if cleaned is False:
-				# Set plot color for the QC flags, and add to plot in a loop
-				color_code = {2:'green',3:'orange',4:'red'}
-				for flag, col in color_code.items():
-					limited_df = df[df[color_column]==flag]
-					ax[count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
-						c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
-				if count == 0:
-					ax[count].legend(fontsize=9, bbox_to_anchor=(1, 1))
-			else:
-				limited_df = df[df[color_column]==2]
-				ax[count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
-					c='green', alpha=0.7, edgecolors='none', marker='.')
-
-			# Add y-axis label, grid and lettering
-			#ax[count].set_ylabel(colname, fontsize=10)
-			ax[count].grid(True)
-			title = letters[count] + ')     ' + colname
-			ax[count].set_title(title, loc='left', fontsize=10,
-					fontweight='bold')
-
-			count += 1
-
-		# Add x-axis sideways
-		fig.autofmt_xdate()
-
-	#------
+	# Set figsize
+	fig_width = 9.5
+	plot_height = 3
+	if n_plot <= plot_limit:
+		fig_height = plot_height*n_plot
 	else:
-		# List of letters used for labelling the plots
-		letters=list(string.ascii_lowercase[0:n_plot])
+		fig_height = plot_height*n_plot/2
+	figsize = (fig_width, fig_height)
 
-		# Setup the plot, and make subplots in a loop
-		fig, ax = plt.subplots(math.ceil(n_plot/2), 2, sharex=True, figsize= (9,(3*n_plot/2)))
-		letter_count = 0
-		row_count = 0
-		col_count = 0
-		for colname in colnames:
+	# List of letters used for labelling the plots
+	letters=list(string.ascii_lowercase[0:n_plot])
 
-			# Remove rows with NaNs
-			df = df.dropna(subset=[colname])
+	# Set up the plot
+	fig, ax = plt.subplots(sharex=True, figsize=figsize)
 
-			# Identify the parameters QC flag columns and set the color code
-			color_column = colname + ' QC Flag'
+	letter_count = 0
+	row_count = 0
+	col_count = 0
+	for colname in colnames:
 
-			# If cleaned is false, plot all data. Else, plot data with QC flag 2.
-			if cleaned is False:
-				# Set plot color for the QC flags, and add to plot in a loop
-				color_code = {2:'green',3:'orange',4:'red'}
-				for flag, col in color_code.items():
-					limited_df = df[df[color_column]==flag]
-					ax[row_count,col_count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
-						c=col, label=flag, alpha=0.7, edgecolors='none', marker='.')
-				if row_count == 0 and col_count == 1:
-					ax[row_count,col_count].legend(fontsize=9, bbox_to_anchor=(1, 1))
-			else:
-				limited_df = df[df[color_column]==2]
-				ax[row_count,col_count].scatter(x=limited_df['Date/Time'], y=limited_df[colname],
-					c='green', alpha=0.7, edgecolors='none', marker='.')
+		# Specify where in the figure to plot the next parameter
+		if n_plot > 1 and n_plot <= 4:
+			ax = plt.subplot2grid((n_plot, 1), (letter_count,0))
+		elif n_plot > 5 and n_plot <= 8:
+			ax = plt.subplot2grid((math.ceil(n_plot/2), 2), (row_count,col_count))
+		else:
+			ax = plt.subplot2grid((math.ceil(n_plot/3), 3), (row_count,col_count))
 
+		# Make subplot
+		make_subplot(colname, df, cleaned, ax)
 
-			# Add y-axis label, grid and lettering
-			ax[row_count,col_count].grid(True)
+		# Add title (and letter if needed)
+		if n_plot == 1:
+			plt.title('     ' + colnames[0], fontsize=10, fontweight='bold')
+		else:
 			title = letters[letter_count] + ')     ' + colname
-			ax[row_count,col_count].set_title(title, loc='left', fontsize=10,
-					fontweight="bold")
+			ax.set_title(title, loc='left', fontsize=10,
+				fontweight='bold')
 
-			# Edit counters
-			letter_count += 1
+		# !!! Increase counters in a better way !!!
+		# Increase counters
+		letter_count += 1
 
+		if n_plot > 5 and n_plot <= 8:
 			if col_count == 1:
 				col_count = 0
 				row_count += 1
 			else:
 				col_count = 1
-
-
-		# Add x-axis sideways
-		fig.autofmt_xdate()
-
-
+		else:
+			if letter_count %3 == 0:
+				row_count += 1
+			if col_count == 0:
+				col_count = 1
+			elif col_count == 1:
+				col_count = 2
+			elif col_count == 2:
+				col_count = 0
 
 	#----------------------
+	# Add x-axis sideways
+	fig.autofmt_xdate()
+
 	# Save plot to file
 	if cleaned is True:
 		filename = 'kpi_plot_data_cleaned.png'
