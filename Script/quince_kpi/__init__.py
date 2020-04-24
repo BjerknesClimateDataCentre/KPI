9b59d87ad0c30a67d1eced491597c789fe281190
Camilla <camilla.landa@uib.no>
2020-04-24 13:09:53 +0200

    (#4) Add kpi pie chart and short name for parameters in config

--- Script/quince_kpi/__init__.py
+++ Script/quince_kpi/__init__.py
@@ -2,6 +2,7 @@ from .dataframe import set_datetime, get_parameters
 from .kpi_line_plot import line_plot
 from .kpi_bar_plot import bar_plot, stacked_bar_plot
 from .introduction_plots import intro_plots
+from .kpi_flag_piechart import flag_piechart
 
 __all__=['set_datetime', 'get_parameters', 'line_plot', 'bar_plot',
-'stacked_bar_plot', 'intro_plots']
+'stacked_bar_plot', 'intro_plots', 'flag_piechart']
---
+++ Script/quince_kpi/kpi_flag_piechart.py
@@ -0,0 +1,45 @@
+###############################################################################
+### KPI: Pie char
+###############################################################################
+
+### Description:
+# Function for creating a pie chart for a single parameter
+
+
+#------------------------------------------------------------------------------
+### Import packages
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+#------------------------------------------------------------------------------
+### Set variables
+size = 4
+
+#------------------------------------------------------------------------------
+### Functions
+
+def flag_piechart(parameter, short_name, df, output_dir, **kwargs):
+
+	# Store the parameters QC flags in a list (remove '.0' if needed)
+	flag_list = list(df[parameter + ' QC Flag'])
+	flag_list_cleaned = [str(item).split('.')[0] for item in flag_list]
+
+	# Get the unique flags and their frequencies
+	flag_freq = np.unique(flag_list_cleaned, return_counts=True)
+	unique_flags = flag_freq[0]
+	freq = flag_freq[1]
+
+	# Create pie chart
+	plt.subplots(figsize=(size,size))
+	patches, texts = plt.pie(freq, startangle=90)
+	plt.legend(patches, unique_flags, loc="best")
+	plt.axis('equal')
+	plt.tight_layout()
+
+	# Save the plot to file
+	filename = short_name + '_flag_piechart.png'
+	filepath = os.path.join(output_dir, filename)
+	plt.savefig(filepath, bbox_inches='tight')
+
+	return filename
