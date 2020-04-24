07d82045a73d463c28a257bf10870bf3ca76b992
Camilla <camilla.landa@uib.no>
2020-04-24 14:10:48 +0200

    (#4) Synchronize color scheme for QC flags

--- Script/quince_kpi/kpi_flag_piechart.py
+++ Script/quince_kpi/kpi_flag_piechart.py
@@ -15,6 +15,7 @@ import numpy as np
 #------------------------------------------------------------------------------
 ### Set variables
 size = 4
+color_dict = {'2':'green','3':'orange','4':'red', 'nan':'grey'}
 
 #------------------------------------------------------------------------------
 ### Functions
@@ -30,9 +31,14 @@ def flag_piechart(parameter, short_name, df, output_dir, **kwargs):
 	unique_flags = flag_freq[0]
 	freq = flag_freq[1]
 
+	# Create color list:
+	color_list = []
+	for flag in unique_flags:
+		color_list.append(color_dict[flag])
+
 	# Create pie chart
 	plt.subplots(figsize=(size,size))
-	patches, texts = plt.pie(freq, startangle=90)
+	patches, texts = plt.pie(freq, colors=color_list, startangle=90)
 	plt.legend(patches, unique_flags, loc="best")
 	plt.axis('equal')
 	plt.tight_layout()
