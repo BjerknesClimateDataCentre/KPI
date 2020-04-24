f326557cb5bde329947f6e2f859239755eb3f002
Camilla <camilla.landa@uib.no>
2020-04-24 10:00:58 +0200

    (#4) Add function for creating introduction plots
    This new function loops through the other introduction plot functions.

--- Script/quince_kpi/__init__.py
+++ Script/quince_kpi/__init__.py
@@ -1,6 +1,7 @@
 from .dataframe import set_datetime, get_parameters
 from .kpi_line_plot import line_plot
 from .kpi_bar_plot import bar_plot, stacked_bar_plot
+from .introduction_plots import intro_plots
 
 __all__=['set_datetime', 'get_parameters', 'line_plot', 'bar_plot',
-'stacked_bar_plot']
+'stacked_bar_plot', 'intro_plots']
