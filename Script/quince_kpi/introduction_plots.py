10137828dc990ad7578fba39ae711c8079d4f92b
Camilla <camilla.landa@uib.no>
2020-04-28 13:16:56 +0200

    (#4) Remove some unnecessary script, and improve some names

--- Script/quince_kpi/introduction_plots.py
+++ Script/quince_kpi/introduction_plots.py
@@ -19,7 +19,7 @@ def intro_plots(intro_plot_config, render_dict, parameters, df, output_dir):
 
 	for kpi_type, config in intro_plot_config.items():
 		if config['include']:
-			filename = 'kpi_' + kpi_type + '_filename'
+			filename = kpi_type + '_filename'
 			render_dict[filename] = eval('kpi.'+ kpi_type)(parameters=parameters,
 				df=df, output_dir=output_dir, kwargs=config['function_input'])
 	return render_dict
