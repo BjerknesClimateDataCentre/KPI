24e3a31dd079c4f9139f4d8faf5c4b5fab942c29
Camilla <camilla.landa@uib.no>
2020-06-08 13:02:08 +0200

    (#10) Allow tabel with more than 2 columns in intro section

--- Script/quince_kpi/intro_tabel.py
+++ Script/quince_kpi/intro_tabel.py
@@ -20,6 +20,8 @@
 def intro_count_tabel(df, intro_section_config):
 
 	# Create a dummy tabel_dict
-	tabel_dict = {'QC Comment': 'Counts', 'test': '2', 'dummy': '3'}
+	tabel_dict = {'1': {'QC Comment': 'temp bad', 'Count':'3', 'Percent':'10'},
+	'2': {'QC Comment': 'co2 bad', 'Count':'6', 'Percent':'24'},
+	'3': {'QC Comment': 'sal bad', 'Count':'1', 'Percent':'9'}}
 
 	return tabel_dict
