583fc2da3ba2927bbec0d1261c621131414a1271
Camilla <camilla.landa@uib.no>
2020-06-10 13:53:23 +0200

    (#10) Rename: replace all 'tabel' with 'table'

--- Script/quince_kpi/__init__.py
+++ Script/quince_kpi/__init__.py
@@ -1,12 +1,12 @@
 from .minor_functions import set_datetime, get_parameters, add_filename, add_number
 from .intro_line import intro_line_plot
 from .intro_bar import intro_bar_plot, intro_stacked_bar_plot
-from .main_functions import intro_figures, intro_tabels, meas_figures, meas_tabels
+from .main_functions import intro_figures, intro_tables, meas_figures, meas_tables
 from .meas_section_kpis import meas_flag_piechart, meas_line_plot, meas_qc_comment_table
-from .intro_tabel import intro_count_tabel
+from .intro_table import intro_count_table
 
 __all__=['set_datetime', 'get_parameters', 'intro_line_plot', 'intro_bar_plot',
 'intro_stacked_bar_plot', 'intro_figures', 'meas_flag_piechart',
 'meas_line_plot', 'meas_figures',
-'meas_qc_comment_table', 'add_filename', 'add_number', 'meas_tabels',
-'intro_count_tabel', 'intro_tabels']
+'meas_qc_comment_table', 'add_filename', 'add_number', 'meas_tables',
+'intro_count_table', 'intro_tables']
