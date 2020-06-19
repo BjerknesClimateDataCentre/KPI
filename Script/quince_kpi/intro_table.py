b45b7ba2768a70e7c5d2f6e736f2469960b1b391
Camilla <camilla.landa@uib.no>
2020-06-19 09:40:43 +0200

    (#10) Simplify table function returns (list, instead of dict, of dicts)

--- Script/quince_kpi/intro_table.py
+++ Script/quince_kpi/intro_table.py
@@ -18,22 +18,12 @@
 ### Functions
 
 def intro_count_table(df, meas_vocab, calc_vocab):
-
-	table_dict = {}
-	row_count = 1
+	table_list = []
 	for sensor_vocab in meas_vocab.values():
-		table_dict[row_count] = {
+		table_list.append({
 			'Parameter': sensor_vocab['subsection_title'],
 			'Short Name': sensor_vocab['fig_label_name_html'],
 			'Parameter Type': 'Measured',
 			'Total Number of Values': len(df[sensor_vocab['col_header_name']])
-			}
-		row_count += 1
-
-	print(table_dict)
-	# Create a dummy table_dict
-	#table_dict = {'1': {'Col1 Name': 'value', 'Col2 Name':'value', 'Col3 Name':'value'},
-	#'2': {'Col1 Name': 'value', 'Col2 Name':'value', 'Col3 Name':'value'},
-	#'3': {'Col1 Name': 'value', 'Col2 Name':'value', 'Col3 Name':'value'}}
-
-	return table_dict
+			})
+	return table_list
--- Script/quince_kpi/main_functions.py
+++ Script/quince_kpi/main_functions.py
@@ -21,9 +21,9 @@ def eval_intro_fig_function(kpi_name, meas_vocab, calc_vocab, df, output_dir):
 
 # Ccreate and return a KPI table for the measured section
 def eval_intro_tab_function(kpi_name, meas_vocab, calc_vocab, df):
-	table_dict = eval('kpi.' + kpi_name)(meas_vocab=meas_vocab,
+	table_list = eval('kpi.' + kpi_name)(meas_vocab=meas_vocab,
 	calc_vocab=calc_vocab, df=df)
-	return table_dict
+	return table_list
 
 # Creates a KPI figure for the measured section and store it in the output dir
 def eval_meas_fig_function(kpi_name, sensor, meas_vocab, df, output_dir):
@@ -32,6 +32,6 @@ def eval_meas_fig_function(kpi_name, sensor, meas_vocab, df, output_dir):
 
 # Ccreate and return a KPI table for the measured section
 def eval_meas_tab_function(kpi_name, sensor, meas_vocab, df):
-	table_dict = eval('kpi.' + kpi_name)(sensor=sensor,
+	table_list = eval('kpi.' + kpi_name)(sensor=sensor,
 		meas_vocab=meas_vocab, df=df)
-	return table_dict
+	return table_list
