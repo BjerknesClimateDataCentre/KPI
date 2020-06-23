6daa302c9cd61e3b6f79b223f22ce09843d113e6
Camilla <camilla.landa@uib.no>
2020-06-23 13:39:45 +0200

    (#10) Cleanup base.html file + add content to calc section

--- Script/quince_kpi/__init__.py
+++ Script/quince_kpi/__init__.py
@@ -1,10 +1,10 @@
+from .eval_functions import eval_intro_fig_function, eval_intro_tab_function, eval_fig_function, eval_tab_function
 from .intro_line import intro_line_plot
 from .intro_bar import intro_bar_plot, intro_stacked_bar_plot
-from .eval_functions import eval_fig_function, eval_tab_function, eval_intro_fig_function, eval_intro_tab_function
-from .meas_section_kpis import meas_flag_piechart, meas_line_plot, meas_qc_comment_table
 from .intro_table import intro_count_table
+from .meas_section_kpis import meas_flag_piechart, meas_line_plot, meas_qc_comment_table
 
-__all__=['intro_line_plot', 'intro_bar_plot',
-'intro_stacked_bar_plot', 'meas_flag_piechart', 'meas_line_plot',
-'meas_qc_comment_table', 'intro_count_table', 'eval_fig_function',
-'eval_tab_function', 'eval_intro_fig_function', 'eval_intro_tab_function']
+__all__ = ['eval_intro_fig_function', 'eval_intro_tab_function',
+	'eval_fig_function', 'eval_tab_function', 'intro_line_plot',
+	'intro_bar_plot', 'intro_stacked_bar_plot', 'intro_count_table',
+	'meas_flag_piechart', 'meas_line_plot', 'meas_qc_comment_table']
