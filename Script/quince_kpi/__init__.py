8414b022d1d01b4acafc86b58d10845744f7a8f4
Camilla <camilla.landa@uib.no>
2020-08-04 12:27:00 +0200

    (#13) QC table is fixed for calculated values section

--- Script/quince_kpi/__init__.py
+++ Script/quince_kpi/__init__.py
@@ -4,9 +4,9 @@ from .overview_bar import overview_bar_plot
 from .overview_table import overview_table
 from .flag_piechart import flag_piechart
 from .line_plot import line_plot
-from .qc_comment_tables import meas_qc_comment_table, calc_qc_comment_table
+from .qc_comment_tables import qc_comment_table
 
 __all__ = ['eval_overview_fig_function', 'eval_overview_tab_function',
 	'eval_fig_function', 'eval_tab_function', 'overview_line_plot',
 	'overview_bar_plot', 'overview_count_table', 'flag_piechart', 'line_plot',
-	'meas_qc_comment_table']
+	'qc_comment_table']
