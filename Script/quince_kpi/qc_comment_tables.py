10a58d2afd9649d89bdc94ad14fba95872c762f9
Camilla <camilla.landa@uib.no>
2020-08-04 12:27:00 +0200

    (#13) QC table is fixed for calculated values section

--- Script/quince_kpi/qc_comment_tables.py
+++ Script/quince_kpi/qc_comment_tables.py
@@ -19,7 +19,7 @@ import numpy as np
 #------------------------------------------------------------------------------
 ### Functions
 
-def meas_qc_comment_table(parameter, vocab, df, var_config):
+def qc_comment_table(parameter, vocab, df, var_config):
 
 	# Store the parameter QC comments in a list and remove nan's
 	col_header_name = vocab[parameter]['col_header_name']
@@ -30,42 +30,9 @@ def meas_qc_comment_table(parameter, vocab, df, var_config):
 	comment_list_cleaned = []
 	for comment in comment_list:
 		if ';' in comment:
-			splitted = comment.split('; ')
-			comment_list_cleaned.extend(splitted)
-		else:
-			comment_list_cleaned.append(comment)
-
-	# Extract unique comments and their frequency and store in a dictonary
-	unique_comments, freq = np.unique(comment_list_cleaned, return_counts=True)
-	table_dict = dict(zip(list(unique_comments), list(freq)))
-
-	# Sort dictionary by the frequency, high to low.
-	table_dict_sorted = {k: v for k, v in sorted(table_dict.items(),
-		key=lambda item: item[1], reverse=True)}
-
-	# Restructure dict to table format and add percentages to each count
-	table_list = []
-	for key, value in table_dict_sorted.items():
-		percent = round((value/df.shape[0])*100,2)
-		table_list.append({'QC Comment': key,
-			'Counts': str(value) + ' (' + str(percent) + '%)'})
-
-	return table_list
-
-
-def calc_qc_comment_table(parameter, vocab, df, var_config):
-
-	# Store the parameter QC comments in a list and remove nan's
-	col_header_name = vocab[parameter]['col_header_name']
-	comment_list = list(df[col_header_name + ' QC Comment'])
-	comment_list = [x for x in comment_list if x == x]
-
-	# Split comments if they contain ';' (means more than one comment in a row)
-	comment_list_cleaned = []
-	for comment in comment_list:
-		if ';' in comment:
-			splitted = comment.split('; ')
-			comment_list_cleaned.extend(splitted)
+			splitted = comment.split(';')
+			for split in splitted:
+				comment_list_cleaned.append(split.lstrip())
 		else:
 			comment_list_cleaned.append(comment)
 
